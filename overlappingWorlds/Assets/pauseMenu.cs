d6f3d4f16d2108c1f46bd48b393d120fcd71a8d1
leotray <ohany656@gmail.com>
2023-05-22 09:12:15 +0300

    made PauseMenu

---
+++ overlappingWorlds/Assets/pauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject pauseMenuUI;
+
+    public void pauseGame(InputAction.CallbackContext context)
+    {
+        
+        if(context.performed)
+        {
+            if(gameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        Application.Quit();
+    }
+    public void mainMenu()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        SceneManager.LoadScene("MainMenu");
+        
+    }
+    
+}
--- overlappingWorlds/Assets/scripts/switchLevels.cs
+++ overlappingWorlds/Assets/scripts/switchLevels.cs
@@ -11,11 +11,7 @@ public class switchLevels : MonoBehaviour
     
 
     public bool canSwitch;
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
+
 
     public void switchWorlds(InputAction.CallbackContext context)
     {
