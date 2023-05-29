c3b48dc8fe1ef01d17ac3eca887cdde18eda43ab
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-05-29 13:56:53 -0700

    good

---
+++ overlappingWorlds/Assets/scripts/GameEndScr.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) // to see when the player enters the collider
+    {
+        if(other.gameObject.tag == "Door") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+          SceneManager.LoadScene("theend");
+        }
+    }
+}
