0034ed65e94968c95056405f6ec55c59441e2db6
Rúben Tadeia <125905559+rubenftgd@users.noreply.github.com>
2023-05-29 17:54:59 +0100

    First build with camera following

--- overlappingWorlds/Assets/scripts/MainMenu.cs
+++ overlappingWorlds/Assets/scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
    public void StartGame()
    {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("Level1");
    }
 
    public void QuitGame()
